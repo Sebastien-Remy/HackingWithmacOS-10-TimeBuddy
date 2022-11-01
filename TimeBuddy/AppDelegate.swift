6afd48ccee1f663dd1ce974dacab2e049d4daa54
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 09:39:26 +0100

    Present content view and close it when needed

--- TimeBuddy/AppDelegate.swift
+++ TimeBuddy/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     
     var statusItem: NSStatusItem?
     var popover = NSPopover()
@@ -17,8 +17,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         statusItem?.button?.title = "Time Buddy"
+        statusItem?.menu = NSMenu()
+        statusItem?.menu?.delegate = self
         
+        
+        let contentView = ContentView()
+        popover.contentSize = CGSize(width: 500, height: 400)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        
+        NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) {
+            [weak self] event in
+            self?.popover.performClose(event)
+        }
     }
     
+    func menuWillOpen(_ menu: NSMenu) {
+        addMenuItems()
+        popover.performClose(self)
+    }
     
+    @objc func ShowSettings() {
+        guard let statusBarButton = statusItem?.button else { return }
+        popover.show(relativeTo: statusBarButton.bounds,
+                     of: statusBarButton,
+                     preferredEdge: .maxY)
+    }
+    
+    func addMenuItems() {
+        statusItem?.menu?.removeAllItems()
+        statusItem?.menu?.addItem(withTitle: "Settings",
+                                  action: #selector(ShowSettings),
+                                  keyEquivalent: "")
+    }
 }
