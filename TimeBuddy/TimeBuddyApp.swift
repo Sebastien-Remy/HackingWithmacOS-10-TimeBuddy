0ce38794465405df7caacec7504e6fe6df74c1e5
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 08:10:41 +0100

    Initial Commit

---
+++ TimeBuddy/TimeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBuddyApp.swift
+//  TimeBuddy
+//
+//  Created by Sebastien REMY on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
